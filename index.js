6d581551ed7da24d15f5af9e9a5876b8d23c41ed
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-13 09:17:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleWire
+app.use(cors())
+app.use(express.json())
+
+
+// =======================mongodb =============================
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://TnmySrkr:<password>@cluster0.cqx0ng3.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// =======================mongodb =============================
+
+app.get('/', (req, res) => {
+    res.send("Simple crud is running")
+})
+
+app.listen(port,()=>{
+    console.log(`simple crud is running on port ${port}`);
+})
+
