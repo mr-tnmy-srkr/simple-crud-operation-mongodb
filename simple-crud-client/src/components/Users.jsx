5a296c9f078fac85126b3700e803f384fd1d7184
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-13 14:21:33 +0530

    delete from ui complete

--- simple-crud-client/src/components/Users.jsx
+++ simple-crud-client/src/components/Users.jsx
@@ -1,23 +1,27 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
 
   const handleDelete = (_id) => {
     console.log("delete", _id);
-    fetch(`http://localhost:5000/users/${_id}`,{
-      method: 'DELETE',
-     /*  headers: {
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+      /*  headers: {
         'Content-Type': 'application/json'
       } */
     })
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data);
-      if(data.deleteCount>0){
-        alert('deleted successfully');
-      }
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      });
   };
 
   console.log(users);
@@ -28,7 +32,7 @@ const Users = () => {
         {users.map((user) => (
           <p key={user._id}>
             {user.name} ::: {user.email} ::: {user._id} :::
-            <button onClick={()=>handleDelete(user._id)}>x</button>
+            <button onClick={() => handleDelete(user._id)}>x</button>
           </p>
         ))}
       </div>
